b873d94a34f1aacf345405d2cdb8f8d7b840ed08
jimini1026 <2004imjimin1>
2023-07-28 00:33:51 +0900

    [기초-조건/선택실행구조]

---
+++ 1068.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+	int a;
+
+	scanf("%d", &a);
+	
+	if (a >= 90)
+		printf("A");
+	else if (a >= 70)
+		printf("B");
+	else if (a >= 40)
+		printf("C");
+	else
+		printf("D");
+
+	return 0;
+}
